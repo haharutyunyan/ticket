4263e45743103f4adc4ab7029b2400c0f5ff2e61
Hayk <Haharutyunyan@yandex.ru>
2017-07-09 01:11:59 +0400

    initial commit1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <link rel="stylesheet" href="{!! asset('css/bootstrap.min.css') !!}" >
+    <link rel="stylesheet" href="{!! asset('css/bootstrap-theme.min.css') !!}">
+    <script src="{!! asset('js/jquery-3.1.1.min.js') !!}"></script>
+    <script src="{!! asset('js/bootstrap.min.js') !!}"></script>
+    <title>Home Page</title>
+
+</head>
+<div class="container">
+    <div class="content">
+        <div class="title">Home Page</div>
+        <div class="quote">Our Home page!</div>
+    </div>
+</div>
+</body>
+</html>
